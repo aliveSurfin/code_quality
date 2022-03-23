3ec965e24f42236bdbcd49d7cb4207aecf716b4c
alivesurfin <alivesurfin@gmail.com>
2022-03-23 18:09:19 +0000

    Added overlay to inform and direct to testing sheet Added language selection for research testing Refactored instances of state manip to be more react friendly removed &amp and &lt checks in text

--- development/code/client/src/components/code_editor/CodeEditor.jsx
+++ development/code/client/src/components/code_editor/CodeEditor.jsx
@@ -17,45 +17,54 @@ import "prismjs/components/prism-regex";
 import "prismjs/components/prism-sql";
 import "prismjs/components/prism-typescript";
 import "prismjs/components/prism-yaml";
-
+import Overlay from "../reusable_comps/overlay/overlay";
+const overlayElement = (
+  <span>
+    {" "}
+    <b>This is a development build for research and testing only </b>
+    <br /> If you have found your way here without reading the participant
+    information,{" "}
+     {/* eslint-disable-next-line */}
+    <a target="_blank"  href="https://forms.office.com/r/f5ef0regrw">
+      please click here
+    </a>{" "}
+  </span>
+);
 // inspired from https://css-tricks.com/creating-an-editable-textarea-that-supports-syntax-highlighted-code/
-
 class CodeEditor extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       text: "",
       highlightingRef: React.createRef(),
       editingRef: React.createRef(),
+      language: props.language || "javascript",
     };
   }
-  componentDidMount() {
-    setTimeout(() => Prism.highlightAll(), 0);
-  }
+
+  componentDidMount() {}
   componentDidUpdate() {
-    setTimeout(() => Prism.highlightAll(), 0);
+    Prism.highlightAll();
   }
   handleInput(event) {
     event.persist(); // stops react from recycling the SyntheticEvent on re-render
     let text = this.state.editingRef.current.value;
-    if (text[text.length - 1] == "\n") {
+    if (text[text.length - 1] === "\n") {
       text += " ";
     }
     this.setState({ text });
   }
   handleScroll(event) {
-    event.persist();
-    this.state.highlightingRef.current.scrollLeft =
-      this.state.editingRef.current.scrollLeft;
-    this.state.highlightingRef.current.scrollTop =
-      this.state.editingRef.current.scrollTop;
+    event.persist(); // stops react from recycling the SyntheticEvent on re-render
+    const highlightingElement = this.state.highlightingRef.current;
+    highlightingElement.scrollLeft = this.state.editingRef.current.scrollLeft;
+    highlightingElement.scrollTop = this.state.editingRef.current.scrollTop;
   }
 
   handleKeyDown(event) {
     if (event.key === "Tab") {
       event.preventDefault();
       this.handleTab();
-
       return;
     }
 
@@ -65,29 +74,27 @@ class CodeEditor extends React.Component {
 
   handleTab() {
     let text = this.state.text;
-
+    const editingElement = this.state.editingRef.current;
     let before = text.slice(0, this.state.editingRef.current.selectionStart);
     let after = text.slice(
       this.state.editingRef.current.selectionEnd,
       text.length
     );
-   
-    let cursor = this.state.editingRef.current.selectionStart + 1;
-    console.log(cursor);
-    
-    let newText = before + "\t" + after;
-    
 
-    this.state.editingRef.current.value = newText;
-    this.setState({ text: newText }, () =>this.state.editingRef.current.setSelectionRange(cursor, cursor));
+    let cursor = this.state.editingRef.current.selectionStart + 1;
+
+    let newText = before + "\t" + after;
+
+    editingElement.value = newText;
+    this.setState({ text: newText }, () =>
+      this.state.editingRef.current.setSelectionRange(cursor, cursor)
+    );
   }
 
   render() {
     return (
-      <div
-        id="codeEditor"
-        className={styles.wrapper}
-      >
+      <div id="codeEditor" className={styles.wrapper}>
+        <Overlay text={overlayElement} />
         <textarea
           ref={this.state.editingRef}
           placeholder="Enter Code Here "
@@ -108,10 +115,11 @@ class CodeEditor extends React.Component {
           className={styles.highlighting}
           aria-hidden="true"
         >
-          <code className="language-javascript" id="highlighting-content">
-            {this.state.text
-              .replace(new RegExp("&", "g"), "&amp;")
-              .replace(new RegExp("<", "g"), "&lt;")}
+          <code
+            className={`language-${this.props.language || this.state.language}`}
+            id="highlighting-content"
+          >
+            {this.state.text}
           </code>
         </pre>
       </div>
