d129d1fdb9e64e418cb6ca114139060dedeba917
alivesurfin <alivesurfin@gmail.com>
2022-03-27 04:49:58 +0100

    Created new cra template and filled with existing comps
    This was to fix a bug where css modules were not being correctly
    packed when in production. Regarding this each scss file was
    changed to end with .module.scss so react could pick up to
    modularise them when packing.

---
+++ development/code/client/src/components/content_layout/ContentLayout.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import CodeEditor from "../code_editor/CodeEditor";
+import styles from "./contentLayout.module.scss";
+class ContentLayout extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      language: "javascript",
+      selector_ref: React.createRef()
+    };
+  }
+
+  render() {
+    return (
+      <div id="content-layout" className={styles.wrapper}>
+        <select style={{fontFamily:"monospace",padding:"10px",fontSize:"200%"}} ref={this.state.selector_ref} name="language" id="language-selector" onChange={()=>{this.forceUpdate()}}>
+          <option value="javascript">JavaScript</option>
+          <option value="java">Java</option>
+          <option value="python">Python</option>
+          <option value="csharp">C#</option>
+          <option value="cpp">C++</option>
+          <option value="c">C</option>
+          <option value="go">Go</option>
+        </select>
+        <CodeEditor language={this.state.selector_ref.current?.value || this.state.language}></CodeEditor>
+      </div>
+    );
+  }
+}
+export default ContentLayout;
